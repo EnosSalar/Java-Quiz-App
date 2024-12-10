342827c123cec548ffee1c8c72e75d225a3aa02a
Enos <Enos.salar.10@gmail.com>
2024-12-10 09:52:15 +0300

    Added new methods to the Test Suite

--- src/test/java/com/vox/drei/QuizTestSuite.java
+++ src/test/java/com/vox/drei/QuizTestSuite.java
@@ -4,6 +4,6 @@ import org.junit.platform.suite.api.SelectClasses;
 import org.junit.platform.suite.api.Suite;
 
 @Suite
-@SelectClasses({AddQuestionTest.class, RemoveQuestionTest.class})
+@SelectClasses({AddQuestionTest.class, RemoveQuestionTest.class , GetQuestionByNumberTest.class, ClearQuestionTest.class})
 public class QuizTestSuite {
 }
